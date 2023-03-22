e29955825fe3bf466d489901bfd1e7a241bea500
hoodiefabio <73169440+hoodiefabio@users.noreply.github.com>
2023-03-22 13:41:16 +0100

    Random spawn and float down

--- Assets/Scripts/BalloonBehaviour.cs
+++ Assets/Scripts/BalloonBehaviour.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class BalloonBehaviour : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        RandomSpawn();
     }
 
+    
     // Update is called once per frame
     void Update()
     {
-        
+       FloatDown();
     }
+    void RandomSpawn()
+    {
+        this.transform.position = new Vector3(Random.Range(-6, 6), 6);
+    }
+
+    void FloatDown()
+    {
+        transform.position -= new Vector3(0,moveSpeed * Time.deltaTime, 0);
+    }
+
 }
